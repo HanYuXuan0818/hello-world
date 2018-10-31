73b2a7c244964a6069d43766086245f699fe1bbc
HanYuXuan0818 <573422184@qq.com>
2018-10-31 18:57:42 +0800

    test commit

---
+++ equation.c
@@ -0,0 +1,16 @@
+//求解如下方程组
+//ax + by = c
+//dx + ey = f
+//注意输入/输出，数据类型
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	double a,b,c,d,e,f;
+	scanf("%1f%1f%1f%1f%1f%1f",&a,&b,&c,&d,&e,&f) ;
+	double x,y;
+	a*x+b*y == c;
+	d*x+e*y == f;
+	printf("%d\n",x,y);
+	return ;
+}
